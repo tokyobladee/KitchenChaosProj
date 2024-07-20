520c7df893156eafa683d56448324e9f8d3975be
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2024-07-21 02:46:19 +0300

    Animations || Cinemachine package

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]private float moveSpeed = 7f; 
+    [SerializeField]private float rotateSpeed = 10f;
+    private bool isWalking;
+    private void Update()
+    {
+        Vector2 inputVector = new Vector2(0f,0f);
+        
+            if(Input.GetKey(KeyCode.W))
+                inputVector.y = + 1;
+            
+            if(Input.GetKey(KeyCode.S))
+                inputVector.y = - 1;
+            
+            if(Input.GetKey(KeyCode.A))
+                inputVector.x = - 1;
+            
+            if(Input.GetKey(KeyCode.D))
+                inputVector.x = + 1;
+
+            inputVector = inputVector.normalized;
+            
+            Vector3 moveDir = new Vector3(inputVector.x,0f,inputVector.y);
+            transform.position += moveDir * moveSpeed * Time.deltaTime;
+
+            isWalking = moveDir != Vector3.zero;
+            transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
+            
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+}
