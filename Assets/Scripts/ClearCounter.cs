703544a4383109ed60e11e89b2802083e8453586
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2024-08-12 01:46:53 +0300

    Swap kitchen object to another clear counter

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,14 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Serialization;
 
 public class ClearCounter : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
-
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
     private KitchenObject kitchenObject;
+
+    private void Update()
+    {
+        if (testing && Input.GetKeyDown(KeyCode.T)){
+            if (kitchenObject != null) {
+                kitchenObject.SetClearCounter(secondClearCounter);
+            }
+        }
+    }
     
     public void Interact()
     {
@@ -17,9 +28,32 @@ public class ClearCounter : MonoBehaviour
         {
             Debug.Log("Interact");
             Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-            kitchenObjectTransform.localPosition = Vector3.zero;
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
         }
-        
+    }
 
+    public Transform GetKitchenObjectFollowTransform()
+    {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject)
+    {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject()
+    {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject()
+    {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject()
+    {
+        return kitchenObject != null;
     }
 }
