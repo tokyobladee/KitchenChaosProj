327f6688f05fc87468b8a19f8c5e1cde70afc0b9
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2024-09-04 21:51:23 +0300

    Complete plate visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+    public class OnIngredientAddedEventArgs : EventArgs{
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
     private List<KitchenObjectSO> KitchenObjectSOList;
@@ -28,6 +33,10 @@ public class PlateKitchenObject : KitchenObject
         }
 
         KitchenObjectSOList.Add(kitchenObjectSO);
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs{
+            kitchenObjectSO = kitchenObjectSO
+        });
+        
         return true;
         
     }
