94a0def1ad11ca7cd1edf9636cf875b95477d28d
tokyobladee <124399165+tokyobladee@users.noreply.github.com>
2024-09-10 18:45:34 +0300

    Game start

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -47,6 +47,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAlternateAction(object sender, EventArgs e)
     {
+        if(!KitchenGameManager.Instance.IsGamePlaying()) return;
+
         if (selectedCounter != null)
         {
             selectedCounter.InteractAlternate(this);
@@ -55,6 +57,8 @@ public class Player : MonoBehaviour, IKitchenObjectParent
 
     private void GameInput_OnInteractAction(object sender, EventArgs e)
     {
+        if(!KitchenGameManager.Instance.IsGamePlaying()) return;
+        
         if (selectedCounter != null)
         {
             selectedCounter.Interact(this);
@@ -98,7 +102,7 @@ public class Player : MonoBehaviour, IKitchenObjectParent
         }
         else { SetSelectedCounter(null); }
         
-        Debug.Log(selectedCounter);
+        //Debug.Log(selectedCounter);
     }
     
     private void HandleMovement()
